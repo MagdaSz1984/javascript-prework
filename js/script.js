2d50fbfd105765e60e6ff716d3d223e3bde53d81
MagdaSz1984 <148221916+MagdaSz1984@users.noreply.github.com>
2023-10-31 22:38:11 +0100

    add new move

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-var computerMove, playerMove;
-computerMove = "kamień";
-printMessage(
-  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
-);
-playerMove = "Papier";
-printMessage("Zagrałam " + playerMove + ". Tak wygrałam!");
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("wylosowana liczba to: " + randomNumber);
+if (randomNumber == "1") {
+  computerMove = "kamień";
+} else if (randomNumber == "2") {
+  computerMove = "papier";
+} else if (randomNumber == "3") {
+  computerMove = "nozyce";
+}
+printMessage("Mój ruch: " + computerMove);
