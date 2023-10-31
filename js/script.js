7542bb2d59c5235bed1285b4688889b0287b1f0d
MagdaSz1984 <148221916+MagdaSz1984@users.noreply.github.com>
2023-10-31 22:51:00 +0100

    add playerMove

--- js/script.js
+++ js/script.js
@@ -7,5 +7,20 @@ if (randomNumber == "1") {
   computerMove = "papier";
 } else if (randomNumber == "3") {
   computerMove = "nozyce";
-}
+} 
 printMessage("Mój ruch: " + computerMove);
+
+
+var playerMove, playerInput;
+playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+console.log("Wpisana odpowiedź to: " + playerInput);
+if (playerInput == "1") {
+  playerMove = "kamień";
+} else if (playerInput == "2") {
+  playerMove = "papier";
+} else if (playerInput == "3") {
+  playerMove = "nozyce";
+} else (playerInput == "1") {
+  playerMove = "kamień";
+}
+printMessage("Twój ruch: " + playerMove);
