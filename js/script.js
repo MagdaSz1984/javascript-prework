95f13ea8474e3175b39c5b01f1458e9669a99345
MagdaSz1984 <148221916+MagdaSz1984@users.noreply.github.com>
2023-11-01 21:28:34 +0100

    add function getMoveName and displayResult

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log("wylosowana liczba to: " + randomNumber);
-if (randomNumber == "1") {
-  computerMove = "kamień";
-} else if (randomNumber == "2") {
-  computerMove = "papier";
-} else if (randomNumber == "3") {
-  computerMove = "nozyce";
-} 
-printMessage("Mój ruch: " + computerMove);
-
-
-var playerMove, playerInput;
-playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
-console.log("Wpisana odpowiedź to: " + playerInput);
-if (playerInput == "1") {
-  playerMove = "kamień";
-} else if (playerInput == "2") {
-  playerMove = "papier";
-} else if (playerInput == "3") {
-  playerMove = "nozyce";
-} else (playerInput == "1") {
-  playerMove = "kamień";
+function getMoveName(argMoveId) {
+  console.log("wywołano funkcję getMoveName z argumentem: " + argMoveId);
+  if (argMoveId == 1) {
+    return "kamień";
+  } else if (argMoveId == 2) {
+    return "papier";
+  } else if (argMoveId == 3) {
+    return "nożyce";
+  } else {
+    printMessage(
+      "Nie znam ruchu o id " + argMoveId + '. Zakładam, że chodziło o "kamień".'
+    );
+    return "kamień";
+  }
 }
-printMessage("Twój ruch: " + playerMove);
+
+function displayResult(argPlayerMove, argComputerMove) {
+  console.log(
+    "wywołano funkcję displayResults z argumentami: " +
+      argPlayerMove +
+      ", " +
+      argComputerMove
+  );
+  if (argPlayerMove == "papier" && argComputerMove == "kamień") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == "nożyce" && argComputerMove == "papier") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == "kamień" && argComputerMove == "nożyce") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == argComputerMove) {
+    printMessage("Remis!");
+  } else {
+    printMessage("Przegrywasz :(");
+  }
+}
